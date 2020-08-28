ed7a9192e0c15a8990bd3c3cf7171b95acee7b0a
m0stafa7med <mostafa.ahmed1093@yahoo.com>
2020-08-28 23:43:42 +0200

    testing routes & APIs

--- app/Http/Controllers/MoviesController.php
+++ app/Http/Controllers/MoviesController.php
@@ -17,6 +17,7 @@ class MoviesController extends Controller
         $popularMovies =Http::withToken('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OWU4NzI5MjdlMjU4MzNkNDViZTRmMWE2ZjU0N2MzMCIsInN1YiI6IjVmM2IxNTY3OGFjM2QwMDAzNTFhYmI1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bHig4yaMYxLOyu14KRavwKnRSg_R6-k5r55PHAk8tng','bearer')
          ->get('https://api.themoviedb.org/3/movie/popular')
          ->json()['results'];
+        //  dd($popularMovies);
 
          $genresArray = Http::withToken('eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4OWU4NzI5MjdlMjU4MzNkNDViZTRmMWE2ZjU0N2MzMCIsInN1YiI6IjVmM2IxNTY3OGFjM2QwMDAzNTFhYmI1MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.bHig4yaMYxLOyu14KRavwKnRSg_R6-k5r55PHAk8tng','bearer')
          ->get('https://api.themoviedb.org/3/genre/movie/list')
